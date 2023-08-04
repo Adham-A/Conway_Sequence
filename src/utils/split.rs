07965102e9a56f62833cfd3fb1189bc6d51a48fb
adhamawad06@gmail.com <adhamawad06@gmail.com>
2023-08-05 00:38:02 +0200

    :recycle: Add functions for conditions

--- src/utils/split.rs
+++ src/utils/split.rs
@@ -23,22 +23,57 @@ pub fn is_valid_split(l: &str, r: &str) -> bool {
 
     if '2' == l_last {
         //2...
-        (r_len > 1 && '1' == r_0 && Some('1') != r_1 && r_1 != r_2) // 1^1 X^1
-            || (r_len > 2 && '1' == r_0 && Some('1') == r_1 && Some('1') == r_2 && Some('1') != r_3) // 1^3
-            || ('3' == r_0 && (r_len < 2 || ( Some('3') != r_1 && (r_2 != r_1 || r_3 != r_1)))) // 3^1 X^!=3
-            || (!['1', '2', '3'].contains(&r_0) && Some(r_0) != r_1 )
+        is_1_1_x_1(r_len, 1, Some(r_0), r_1, r_2)
+            || is_1_3(Some(r_0), r_1, r_2, r_3)
+            || is_3_1_x_not_3(r_len, 2, Some(r_0), r_1, r_2, r_3)
+            || (!['1', '2', '3'].contains(&r_0) && Some(r_0) != r_1)
     } else if '2' != l_last && '2' == r_0 && Some('2') == r_1 {
         //...22
-        (r_len > 3 && (Some('1') == r_2 && Some('1') != r_3  && r_4 != r_3)) //1^1 X^1
-            || (Some('1') == r_2 && Some('1') != r_3 && r_4 != r_3) // 1^3
-            || Some('3') == r_2 && (r_len < 3 || (Some('3') != r_3 && (r_4 != r_3 || r_5 != r_3))) // 3^1 X^!=3
-            || (r_len == 2||( r_len > 3 && ![Some('1'), Some('2'), Some('3')].contains(&r_2) && r_2 != r_3))
+        is_1_1_x_1(r_len, 3, r_2, r_3, r_4)
+            || is_1_3(r_2, r_3, r_4, r_5)
+            || is_3_1_x_not_3(r_len, 3, r_2, r_3, r_4, r_5)
+            || (r_len == 2
+                || (r_len > 3 && ![Some('1'), Some('2'), Some('3')].contains(&r_2) && r_2 != r_3))
     //n^0 or 1
     } else {
         false
     }
 }
 
+pub fn is_1_1_x_1(
+    string_len: usize,
+    goal_len: usize,
+    char_0: Option<char>,
+    char_1: Option<char>,
+    char_2: Option<char>,
+) -> bool {
+    let o_1 = Some('1');
+    string_len > goal_len && (o_1 == char_0 && o_1 != char_1 && char_2 != char_1)
+}
+
+pub fn is_1_3(
+    char_0: Option<char>,
+    char_1: Option<char>,
+    char_2: Option<char>,
+    char_3: Option<char>,
+) -> bool {
+    let o_1 = Some('1');
+    o_1 == char_0 && o_1 == char_1 && o_1 == char_2 && char_3 != o_1
+}
+
+pub fn is_3_1_x_not_3(
+    string_len: usize,
+    goal_len: usize,
+    char_0: Option<char>,
+    char_1: Option<char>,
+    char_2: Option<char>,
+    char_3: Option<char>,
+) -> bool {
+    //             || ('3' == r_0 && (r_len < 2 || ( Some('3') != r_1 && (r_2 != r_1 || r_3 != r_1)))) // 3^1 X^!=3
+    let o_3 = Some('3');
+    o_3 == char_0
+        && (string_len < goal_len || (o_3 != char_1 && (char_2 != char_1 || char_3 != char_1)))
+}
 #[cfg(test)]
 mod tests {
     use crate::split::is_valid_split;
@@ -52,6 +87,7 @@ mod tests {
 
     #[test_case("2", "12")]
     #[test_case("2", "111")]
+    #[test_case("2", "1112")]
     #[test_case("2", "3")]
     #[test_case("2", "31")]
     #[test_case("2", "311")]
@@ -77,23 +113,31 @@ mod tests {
         assert!(!is_valid_split(l, r))
     }
 
-    #[test_case("22", "1" ; "2^2X^1")]
-    #[test_case("12" , "22"; "2^3")]
-    #[test_case("311222"  , "22"; "2^5")]
-    #[test_case("3112"  , "22"; "2^3 bis")]
-
-    fn string_split_22_invalid(l: &str, r: &str) {
-        assert!(!is_valid_split(l, r))
-    }
     #[test_case("311", "2213")]
     #[test_case("111", "22")]
+    #[test_case("2111", "22")]
     #[test_case("3", "22")]
     #[test_case("31", "22")]
     #[test_case("311", "22")]
     #[test_case("4", "22")]
     #[test_case("11", "22")]
+    #[test_case("1111", "22")]
+    #[test_case("1", "22111")]
+    #[test_case("111", "22111")]
+    #[test_case("1111", "22111")]
 
     fn string_split_22_valid(l: &str, r: &str) {
         assert!(is_valid_split(l, r))
+    }
+
+    #[test_case("22", "1")]
+    #[test_case("12", "22")]
+    #[test_case("311222", "22")]
+    #[test_case("3112", "22")]
+    #[test_case("1", "221111")]
+    #[test_case("1111", "221111")]
+
+    fn string_split_22_invalid(l: &str, r: &str) {
+        assert!(!is_valid_split(l, r))
     }
 }
