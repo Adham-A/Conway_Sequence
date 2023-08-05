7b1e5a72752a7c78222601ffea3a67e0127bc357
adhamawad06@gmail.com <adhamawad06@gmail.com>
2023-08-05 23:26:50 +0200

    feat: :bug: Add sequence element lookup, and fix periods

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,11 @@ pub use generators::smart::SmartGenerator;
 
 mod lookups {
     pub mod element_decay;
+    pub mod sequence_element;
 }
 
 pub use lookups::element_decay::ELEMENT_DECAY_LOOKUP;
-
+pub use lookups::sequence_element::SEQUENCE_ELEMENT_LOOKUP;
 mod utils {
     pub mod split;
     pub mod sub_sequence;
